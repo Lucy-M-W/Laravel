86cc43ad0b11cc1db50f20122dd96c0e17ba534d
lucy <luciewambui12@gmail.com>
2015-10-14 18:46:49 +0300

    changes

--- resources/views/zipcode_form.blade.php
+++ resources/views/zipcode_form.blade.php
@@ -1,8 +1,19 @@
- <div class="search">
-      
-	    {{ Form::model(null, array('route' => array('Zipcode_code'))) }}
-	    {{ Form::text('query', null, array( 'placeholder' => 'Search query...' )) }}
-	    {{ Form::submit('Search') }}
-	    {{ Form::close() }}
-	</div>
+ <form action="<?php echo e(url('zip_codes')); ?>" method="get" accept-charset="utf-8">
+  
+  <label for="City">City</label>
+   <br>
+  <input type="City" name="City" value="" id="City">
+  <br>
+  <label for="State">State</label>
+   <br>
+  <input type="State" name="State" value="" id="State">
+   <br>
+  <label for="Zip">Zip</label>
+   <br>
+  <input type="Zip" name="Zip" value="" id="Zip">
+  <br>
+  <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>" id="">
+
+     <p><input type="submit" value="search"></p>
+</form>
 
